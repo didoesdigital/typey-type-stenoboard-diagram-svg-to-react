4622696bd957f4fc76e6fa31580884c620339c59
Di <didoesdigital+git@gmail.com>
2018-11-19 10:33:49 +1100

    Update file

--- prepare-optimized-svg-for-react-component.rb
+++ prepare-optimized-svg-for-react-component.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/env ruby
 
-puts "test"
+if ARGV.size < 2
+  $stderr.puts "Usage: ruby ./prepare-optimized-svg-for-react-component.rb STENO_LAYOUT.svg STENOLAYOUTStenoDiagram.js"
+  exit 1
+end
+
+SOURCE_SVG = ARGV[0]
+TARGET_JS = ARGV[1]
 
 
