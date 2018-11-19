9f35dbfb3e08bad4cb8ed0011172d558326e51f4
Di <didoesdigital+git@gmail.com>
2018-11-19 11:00:13 +1100

    Write out JS file and add aria-hidden={hidden}

--- prepare-optimized-svg-for-react-component.rb
+++ prepare-optimized-svg-for-react-component.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
 if ARGV.size < 2
   $stderr.puts "Usage: ruby ./prepare-optimized-svg-for-react-component.rb STENO_LAYOUT.svg STENOLAYOUTStenoDiagram.js"
   exit 1
@@ -8,4 +9,18 @@ end
 SOURCE_SVG = ARGV[0]
 TARGET_JS = ARGV[1]
 
+File.open(TARGET_JS, 'w:utf-8') do |target|
+  File.open(SOURCE_SVG, 'r:utf-8') do |source|
 
+    source.each_line do |raw_line|
+      line = raw_line.rstrip
+      if line =~ /<svg (.+)>/i
+        target.puts "<svg " + $1 + " aria-hidden={hidden}>"
+      else
+        target.puts line
+      end
+    end
+
+  end
+
+end
