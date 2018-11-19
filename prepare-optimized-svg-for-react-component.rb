e21856f808559535daec893923f3ece6ce2942bb
Di <didoesdigital+git@gmail.com>
2018-11-19 11:32:24 +1100

    Use nokogiri to add width, aria-hidden, and remove title

--- prepare-optimized-svg-for-react-component.rb
+++ prepare-optimized-svg-for-react-component.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+require 'nokogiri'
 if ARGV.size < 2
   $stderr.puts "Usage: ruby ./prepare-optimized-svg-for-react-component.rb STENO_LAYOUT.svg STENOLAYOUTStenoDiagram.js"
   exit 1
@@ -9,18 +10,15 @@ end
 SOURCE_SVG = ARGV[0]
 TARGET_JS = ARGV[1]
 
+@doc = File.open(SOURCE_SVG) { |f| Nokogiri::XML(f) }
+
+svg = @doc.at_css "svg"
+svg["aria-hidden"] = "hidden"
+svg["width"] = 140
+
+title = @doc.at_css "title"
+title.remove
+
 File.open(TARGET_JS, 'w:utf-8') do |target|
-  File.open(SOURCE_SVG, 'r:utf-8') do |source|
-
-    source.each_line do |raw_line|
-      line = raw_line.rstrip
-      if line =~ /<svg (.+)>/i
-        target.puts "<svg " + $1 + " aria-hidden={hidden}>"
-      else
-        target.puts line
-      end
-    end
-
-  end
-
+  target.puts @doc.to_html
 end
