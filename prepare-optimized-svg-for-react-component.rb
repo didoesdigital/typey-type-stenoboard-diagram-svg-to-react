0d83ca96fcd8035c791b27539ebebbce5621dc11
Di <didoesdigital+git@gmail.com>
2018-11-19 13:32:57 +1100

    Add path variables

--- prepare-optimized-svg-for-react-component.rb
+++ prepare-optimized-svg-for-react-component.rb
@@ -22,6 +22,7 @@ title.remove
 
 # if title.contents == "italian-steno" then
 g = @doc.at_css "g"
+g_id = g["id"]
 g["transform"] = "translate(1 1)"
 g["id"] = "xxxstenoboard-xxx + this.props.brief xxx}"
 # end
@@ -29,24 +30,38 @@ g["id"] = "xxxstenoboard-xxx + this.props.brief xxx}"
 vars = {}
 rects = @doc.css "rect"
 rects.each do | rect |
-  id = rect["id"]
+  rect_id = rect["id"]
 
   # strokes
   stroke = rect["stroke"]
-  stroke_var_name = id + "StrokeColor"
+  stroke_var_name = rect_id + "StrokeColor"
   stroke_var_value = stroke
   rect["stroke"] = "xxx{" + stroke_var_name + "xxx}"
   vars.store(stroke_var_name, stroke_var_value)
 
   # fills
   fill = rect["fill"]
-  fill_var_name_on = id + "OnColor"
-  fill_var_name_off = id + "OffColor"
+  fill_var_name_on = rect_id + "OnColor"
+  fill_var_name_off = rect_id + "OffColor"
   fill_var_value = fill
-  rect["fill"] = "xxx{this.props." + id + " ? " + fill_var_name_on + " : " + fill_var_name_off + "xxx}"
+  rect["fill"] = "xxx{this.props." + rect_id + " ? " + fill_var_name_on + " : " + fill_var_name_off + "xxx}"
   vars.store(fill_var_name_on, fill_var_value)
   vars.store(fill_var_name_off, fill_var_value)
 end
+paths = @doc.css "path"
+paths.each do | path |
+  path_id = path["id"]
+
+  # fills
+  fill = path["fill"]
+  fill_var_name_on = path_id + "OnTextColor"
+  fill_var_name_off = path_id + "OffTextColor"
+  fill_var_value = fill
+  path["fill"] = "xxx{this.props." + path_id + " ? " + fill_var_name_on + " : " + fill_var_name_off + "xxx}"
+  vars.store(fill_var_name_on, fill_var_value)
+  vars.store(fill_var_name_off, fill_var_value)
+end
+
 File.open(TARGET_JS, 'w:utf-8') do |target|
   target.puts @doc.to_html
 end
@@ -66,6 +81,11 @@ File.open(TARGET_JS, 'w:utf-8') do |target|
 end
 
 remove_first_line = `sed -i '' '1d' #{TARGET_JS}`
+# yarn run v1.9.4
+
 remove_second_line = `sed -i '' '1d' #{TARGET_JS}`
+# $ svg-to-jsx target-js/ItalianMichelaStenoDiagram.js
+
 remove_last_line = `sed -i '' -e '$ d' #{TARGET_JS}`
+# Done in 0.31s.
 
